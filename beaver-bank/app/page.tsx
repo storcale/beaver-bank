ee406128230153c9f78da9975affa7d981e631a7
Storcale <105071746+storcale@users.noreply.github.com>
2024-10-19 22:49:38 +0200

    main page + start signup

--- beaver-bank/app/page.tsx
+++ beaver-bank/app/page.tsx
@@ -8,22 +8,22 @@ export default function IndexPage() {
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          Beautifully designed components <br className="hidden sm:inline" />
-          built with Radix UI and Tailwind CSS.
+          Your new solution for secure,<br className="hidden sm:inline" />
+	        fast and modern banking.
         </h1>
         <p className="max-w-[700px] text-lg text-muted-foreground">
-          Accessible and customizable components that you can copy and paste
-          into your apps. Free. Open Source. And Next.js 13 Ready.
+          Payments delivered straight from the river in seconds,<br className="hidden sm:inline" />
+          no water leaks or half logs delivered
         </p>
       </div>
       <div className="flex gap-4">
         <Link
-          href={siteConfig.links.docs}
+          href="/signup"
           target="_blank"
           rel="noreferrer"
           className={buttonVariants()}
         >
-          Documentation
+          Sign-Up
         </Link>
         <Link
           target="_blank"
---
+++ beaver-bank/app/signup/page.tsx
@@ -0,0 +1,8 @@
+
+export default function SignUp() {
+  return (
+      <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
+        <h1>Create an account</h1>
+      </section>
+  );
+}
